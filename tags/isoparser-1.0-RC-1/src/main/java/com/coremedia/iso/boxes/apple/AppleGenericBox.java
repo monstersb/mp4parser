508bb3a683a83eac559e297b17fde8f7bd89a39d
Sebastian.Annies <Sebastian.Annies@gmail.com>
2012-03-24 08:26:40 +0000

    [maven-release-plugin]  copy for tag isoparser-1.0-RC-1

---
+++ tags/isoparser-1.0-RC-1/src/main/java/com/coremedia/iso/boxes/apple/AppleGenericBox.java
@@ -0,0 +1,15 @@
+package com.coremedia.iso.boxes.apple;
+
+import com.coremedia.iso.boxes.AbstractContainerBox;
+
+/**
+ *
+ */
+public final class AppleGenericBox extends AbstractContainerBox {
+    public static final String TYPE = "----";
+
+    public AppleGenericBox() {
+        super(TYPE);
+    }
+
+}

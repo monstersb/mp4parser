fcb6de0c3c8087b95a066f49b9fca25ccb485e85
Sebastian.Annies <Sebastian.Annies@gmail.com>
2013-04-10 09:20:49 +0000

    [maven-release-plugin]  copy for tag mp4parser-project-1.0-RC-23

---
+++ tags/mp4parser-project-1.0-RC-23/examples/src/main/java/com/googlecode/mp4parser/AppendExample.java
@@ -0,0 +1,68 @@
+package com.googlecode.mp4parser;
+
+import com.coremedia.iso.IsoFile;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ */
+public class AppendExample {
+    public static void main(String[] args) throws IOException {
+
+
+
+        String f1 = AppendExample.class.getProtectionDomain().getCodeSource().getLocation().getFile() + "/1365070268951.mp4";
+        String f2 = AppendExample.class.getProtectionDomain().getCodeSource().getLocation().getFile() + "/1365070285923.mp4";
+        String f3 = AppendExample.class.getProtectionDomain().getCodeSource().getLocation().getFile() + "/1365070453555.mp4";
+
+        Movie[] inMovies = new Movie[]{
+                MovieCreator.build(new FileInputStream(f1).getChannel()),
+                MovieCreator.build(new FileInputStream(f2).getChannel()),
+                MovieCreator.build(new FileInputStream(f3).getChannel())};
+
+        List<Track> videoTracks = new LinkedList<Track>();
+        List<Track> audioTracks = new LinkedList<Track>();
+
+        for (Movie m : inMovies) {
+            for (Track t : m.getTracks()) {
+                if (t.getHandler().equals("soun")) {
+                    audioTracks.add(t);
+                }
+                if (t.getHandler().equals("vide")) {
+                    videoTracks.add(t);
+                }
+            }
+        }
+
+        Movie result = new Movie();
+
+        if (audioTracks.size() > 0) {
+            result.addTrack(new AppendTrack(audioTracks.toArray(new Track[audioTracks.size()])));
+        }
+        if (videoTracks.size() > 0) {
+            result.addTrack(new AppendTrack(videoTracks.toArray(new Track[videoTracks.size()])));
+        }
+
+        IsoFile out = new DefaultMp4Builder().build(result);
+
+        FileChannel fc = new RandomAccessFile(String.format("output.mp4"), "rw").getChannel();
+        fc.position(0);
+        out.getBox(fc);
+        fc.close();
+
+
+    }
+
+
+}

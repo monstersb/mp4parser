7fde97d6a90a3cc09024193ace0fb2d7d94a78fe
sebastian.annies@gmail.com <sebastian.annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-06-03 09:55:33 +0000

    Adopted mp4inspector to new API

--- trunk/tools/mp4-inspector/src/main/java/com/googlecode/mp4parser/mp4inspector/IsoFileTreeView.java
+++ trunk/tools/mp4-inspector/src/main/java/com/googlecode/mp4parser/mp4inspector/IsoFileTreeView.java
@@ -2,22 +2,34 @@ package com.googlecode.mp4parser.mp4inspector;
 
 import com.coremedia.iso.IsoFile;
 import com.coremedia.iso.boxes.Box;
-import com.coremedia.iso.boxes.ContainerBox;
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.util.Path;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
-import javafx.scene.Node;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.util.Callback;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 
 public class IsoFileTreeView extends TreeView<Box> {
 
     public IsoFileTreeView() {
-        setCellFactory(new BoxToStringCallback());
+        setCellFactory(new Callback<TreeView<Box>,TreeCell<Box>>(){
+            @Override
+            public TreeCell<Box> call(TreeView<Box> p) {
+                return new BoxTreeCellImpl();
+            }
+        });
         setShowRoot(false);
+        setEditable(false);
     }
 
     public String getOpenFile() {
@@ -41,12 +53,61 @@ public class IsoFileTreeView extends TreeView<Box> {
             map = new HashMap<String, Object>();
             setUserData(map);
         }
-        IsoFile isoFile = new IsoFile(file);
-        setRoot(createNode(isoFile));
+        final IsoFile isoFile = new IsoFile(file);
+        setRoot(new TreeItem<Box>() {
+            public boolean isFirstTimeChildren = true;
+
+            @Override
+            public boolean isLeaf() {
+                return false;
+            }
+
+            public ObservableList<TreeItem<Box>> getChildren() {
+                if (isFirstTimeChildren) {
+                    isFirstTimeChildren = false;
+                    buildChildren(super.getChildren());
+                }
+                return super.getChildren();
+            }
+
+            private void buildChildren(final ObservableList<TreeItem<Box>> children) {
+
+                    final Iterator<Box> boxes = isoFile.getBoxes().iterator();
+                    int i = 100;
+                    while (boxes.hasNext() && i-- > 0) {
+                        // show 100 now and the rest later
+                        children.add(createNode(boxes.next()));
+                    }
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            while (boxes.hasNext()) {
+                                int i = 100;
+                                List<TreeItem<Box>> treeItemList = new LinkedList<TreeItem<Box>>();
+                                while (boxes.hasNext() && i-- > 0) {
+                                    // show 100 now and the rest later
+                                    treeItemList.add(createNode(boxes.next()));
+                                }
+                                final TreeItem<Box> treeItems[] = treeItemList.toArray(new TreeItem[treeItemList.size()]);
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        children.addAll(treeItems);
+                                    }
+                                });
+                            }
+                        }
+                    }.start();
+                }
+
+
+        });
+
         map.put("isoFile", isoFile);
         map.put("file", file);
 
     }
+
 
     public TreeItem<Box> createNode(final Box b) {
         return new TreeItem<Box>(b) {
@@ -65,12 +126,13 @@ public class IsoFileTreeView extends TreeView<Box> {
                 return super.getChildren();
             }
 
+
             @Override
             public boolean isLeaf() {
                 if (isFirstTimeLeaf) {
                     isFirstTimeLeaf = false;
 
-                    isLeaf = !(getValue() instanceof ContainerBox);
+                    isLeaf = !(getValue() instanceof Container);
                 }
 
                 return isLeaf;
@@ -78,32 +140,60 @@ public class IsoFileTreeView extends TreeView<Box> {
 
             private void buildChildren(final ObservableList<TreeItem<Box>> children) {
                 Box b = this.getValue();
-                if (b != null && b instanceof ContainerBox) {
-                    final Iterator<Box> boxes = ((ContainerBox) b).getBoxes().iterator();
+                if (b != null && b instanceof Container) {
+                    final Iterator<Box> boxes = ((Container) b).getBoxes().iterator();
                     int i = 100;
                     while (boxes.hasNext() && i-- > 0) {
+                        // show 100 now and the rest later
                         children.add(createNode(boxes.next()));
                     }
-                    if (boxes.hasNext()) {
-                        Task<Integer> task = new Task<Integer>() {
-                            @Override
-                            protected Integer call() throws Exception {
-                                System.err.println("call called");
-                                while (boxes.hasNext()) {
-                                    children.add(createNode(boxes.next()));
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            while (boxes.hasNext()) {
+                                int i = 100;
+                                List<TreeItem<Box>> treeItemList = new LinkedList<TreeItem<Box>>();
+                                while (boxes.hasNext() && i-- > 0) {
+                                    // show 100 now and the rest later
+                                    treeItemList.add(createNode(boxes.next()));
                                 }
-                                System.err.println("call ended");
-                                return 0;
+                                final TreeItem<Box> treeItems[] = treeItemList.toArray(new TreeItem[treeItemList.size()]);
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        children.addAll(treeItems);
+                                    }
+                                });
                             }
-                        };
-                        new Thread(task).start();
-                        System.err.println("Start returned");
-                    }
+                        }
+                    }.start();
+
                 }
             }
         };
     }
 
+    private final class BoxTreeCellImpl extends TreeCell<Box> {
+
+        public BoxTreeCellImpl() {
+        }
 
 
+        @Override
+        public void updateItem(Box item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                setText(getString());
+                setGraphic(getTreeItem().getGraphic());
+            }
+        }
+
+        private String getString() {
+            return getItem() == null ? "" : Path.createPath(getItem());
+        }
+    }
 }

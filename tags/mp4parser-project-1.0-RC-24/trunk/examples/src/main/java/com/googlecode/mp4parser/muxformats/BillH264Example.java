05b6ee5c2f78e80fbc0f33c3d88ef7232903dad3
Sebastian.Annies <Sebastian.Annies@gmail.com>
2013-06-30 23:31:29 +0000

    [maven-release-plugin]  copy for tag mp4parser-project-1.0-RC-24

---
+++ tags/mp4parser-project-1.0-RC-24/trunk/examples/src/main/java/com/googlecode/mp4parser/muxformats/BillH264Example.java
@@ -0,0 +1,37 @@
+package com.googlecode.mp4parser.muxformats;
+
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.tracks.AACTrackImpl;
+import com.googlecode.mp4parser.authoring.tracks.H264TrackImpl;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * user: sannies
+ */
+public class BillH264Example {
+    public static void main(String[] args) throws IOException {
+        FileInputStream video_file = new FileInputStream("c:/dev/mp4parser2/source_video.h264");
+        FileInputStream audio_file = new FileInputStream("c:/dev/mp4parser2/source_audio.aac");
+        int duration = 30472;
+        H264TrackImpl h264Track = new H264TrackImpl(video_file.getChannel(), "eng", 15000, 1001); //supplied duration for the attached file was
+        AACTrackImpl aacTrack = new AACTrackImpl(audio_file.getChannel());
+        Movie movie = new Movie();
+        movie.addTrack(h264Track);
+        //movie.addTrack(aacTrack);
+
+        Container out = new DefaultMp4Builder().build(movie);
+        FileOutputStream fos = new FileOutputStream(new File("c:/dev/mp4parser2/checkme.mp4"));
+        out.writeContainer(fos.getChannel());
+        fos.close();
+
+    }
+}

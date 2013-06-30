1bffc36d43334e82689da27608c2b4e29d5bbfa4
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-06-30 23:41:52 +0000

    [maven-release-plugin]  copy for tag mp4parser-project-1.0-RC-24

---
+++ tags/mp4parser-project-1.0-RC-24/examples/src/main/java/com/googlecode/mp4parser/ExtractRawH264.java
@@ -0,0 +1,75 @@
+package com.googlecode.mp4parser;
+
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.IsoTypeReaderVariable;
+import com.coremedia.iso.boxes.Container;
+import com.coremedia.iso.boxes.TrackBox;
+import com.coremedia.iso.boxes.h264.AvcConfigurationBox;
+import com.coremedia.iso.boxes.mdat.SampleList;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.tracks.H264TrackImpl;
+import com.googlecode.mp4parser.util.Path;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+
+public class ExtractRawH264 {
+    public static void main(String[] args) throws IOException {
+        H264TrackImpl h264Track = new H264TrackImpl(new FileInputStream("C:\\dev\\mp4parser\\isoparser\\src\\test\\resources\\count.h264").getChannel());
+        Movie m = new Movie();
+        m.addTrack(h264Track);
+
+
+        Container out = new DefaultMp4Builder().build(m);
+        FileOutputStream fos = new FileOutputStream(new File("output.mp4"));
+        out.writeContainer(fos.getChannel());
+        fos.close();
+        // Up until here we mux a raw h264 file into mp4 and in the next step we regenerate the raw file.
+
+        IsoFile isoFile = new IsoFile("output.mp4");
+        TrackBox trackBox = (TrackBox) Path.getPath(isoFile, "/moov/trak/mdia/minf/stbl/stsd/avc1/../../../../../");
+        SampleList sl = new SampleList(trackBox);
+
+
+        FileChannel fc = new FileOutputStream("out.h264").getChannel();
+        ByteBuffer separator = ByteBuffer.wrap(new byte[]{0, 0, 0, 1});
+
+        fc.write((ByteBuffer) separator.rewind());
+        // Write SPS
+        fc.write(ByteBuffer.wrap(
+                ((AvcConfigurationBox) Path.getPath(trackBox, "mdia/minf/stbl/stsd/avc1/avcC")
+                ).getSequenceParameterSets().get(0)));
+        // Warning:
+        // There might be more than one SPS (I've never seen that but it is possible)
+
+        fc.write((ByteBuffer) separator.rewind());
+        // Write PPS
+        fc.write(ByteBuffer.wrap(
+                ((AvcConfigurationBox) Path.getPath(trackBox, "mdia/minf/stbl/stsd/avc1/avcC")
+                ).getPictureParameterSets().get(0)));
+        // Warning:
+        // There might be more than one PPS (I've never seen that but it is possible)
+
+        int lengthSize = ((AvcConfigurationBox) Path.getPath(trackBox, "mdia/minf/stbl/stsd/avc1/avcC")).getLengthSizeMinusOne() + 1;
+        for (ByteBuffer sample : sl) {
+            while (sample.remaining() > 0) {
+                int length = (int) IsoTypeReaderVariable.read(sample, lengthSize);
+                fc.write((ByteBuffer) separator.rewind());
+                fc.write((ByteBuffer) sample.slice().limit(length));
+                sample.position(sample.position() + length);
+            }
+
+
+        }
+        fc.close();
+
+    }
+
+
+}
---
+++ tags/mp4parser-project-1.0-RC-24/isoparser/src/test/java/com/coremedia/drm/packager/isoparser/RoundTripTest.java
@@ -0,0 +1,144 @@
+/*  
+ * Copyright 2008 CoreMedia AG, Hamburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an AS IS BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+
+package com.coremedia.drm.packager.isoparser;
+
+import com.coremedia.iso.IsoFile;
+import com.googlecode.mp4parser.authoring.tracks.BoxComparator;
+import junit.framework.TestCase;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * Tests ISO Roundtrip.
+ */
+public class RoundTripTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+/*        Logger.getLogger("").setLevel(Level.ALL);
+    Handler[] handlers = Logger.getLogger("").getHandlers();
+    for (Handler handler : handlers) {
+      handler.setLevel(Level.ALL);
+    }*/
+    }
+
+    /*    public void testRoundDeleteMe() throws Exception {
+        testRoundTrip_1("/suckerpunch-distantplanet_h1080p.mov");
+    }*/
+    public void testRoundTrip_TinyExamples_Old() throws Exception {
+        testRoundTrip_1("/Tiny Sample - OLD.mp4");
+    }
+
+    public void testRoundTrip_TinyExamples_Metaxed() throws Exception {
+        testRoundTrip_1("/Tiny Sample - NEW - Metaxed.mp4");
+    }
+
+    public void testRoundTrip_TinyExamples_Untouched() throws Exception {
+        testRoundTrip_1("/Tiny Sample - NEW - Untouched.mp4");
+    }
+
+
+    public void testRoundTrip_1a() throws Exception {
+        testRoundTrip_1("/multiTrack.3gp");
+    }
+
+    public void testRoundTrip_1b() throws Exception {
+        testRoundTrip_1("/MOV00006.3gp");
+    }
+
+    public void testRoundTrip_1c() throws Exception {
+        testRoundTrip_1("/Beethoven - Bagatelle op.119 no.11 i.m4a");
+    }
+
+    public void testRoundTrip_1d() throws Exception {
+        testRoundTrip_1("/test.m4p");
+    }
+
+    public void testRoundTrip_1e() throws Exception {
+        testRoundTrip_1("/test-pod.m4a");
+    }
+
+    public void testRoundTrip_1(String resource) throws Exception {
+
+        long start1 = System.currentTimeMillis();
+        String originalFile = this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile() + resource;
+
+
+        long start2 = System.currentTimeMillis();
+
+        IsoFile isoFile = new IsoFile(originalFile);
+
+        long start3 = System.currentTimeMillis();
+
+
+        long start4 = System.currentTimeMillis();
+        Walk.through(isoFile);
+        long start5 = System.currentTimeMillis();
+
+        File result = File.createTempFile(this.getName(), resource.replace("/", "_"));
+
+        FileOutputStream fos = new FileOutputStream(result);
+        FileChannel fcOut = fos.getChannel();
+        isoFile.getBox(fcOut);
+        fcOut.close();
+        fos.close();
+
+
+        long start6 = System.currentTimeMillis();
+
+     /*   System.err.println("Preparing tmp copy took: " + (start2 - start1) + "ms");
+        System.err.println("Parsing took           : " + (start3 - start2) + "ms");
+        System.err.println("Writing took           : " + (start6 - start3) + "ms");
+        System.err.println("Walking took           : " + (start5 - start4) + "ms");*/
+
+
+        IsoFile copyViaIsoFileReparsed = new IsoFile(result.getAbsolutePath());
+        BoxComparator.check(isoFile, copyViaIsoFileReparsed, "/moov[0]/mvhd[0]", "/moov[0]/trak[0]/tkhd[0]", "/moov[0]/trak[0]/mdia[0]/mdhd[0]");
+        isoFile.close();
+        copyViaIsoFileReparsed.close();
+        result.deleteOnExit();
+        // as windows cannot delete file when something is memory mapped and the garbage collector
+        // doesn't necessarily free the Buffers quickly enough we cannot delete the file here (we could but only for linux)
+
+
+    }
+
+
+    public static void copy(InputStream input, OutputStream output) throws IOException {
+        assert input != null && output != null;
+
+        byte[] buffer = new byte[4096];
+        int count = input.read(buffer);
+        while (count > 0) {
+            output.write(buffer, 0, count);
+            count = input.read(buffer);
+        }
+    }
+}

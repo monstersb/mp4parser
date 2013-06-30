4c58c6f389c996326a3bdf1ea8a6c497eb29660c
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-06-30 23:31:29 +0000

    [maven-release-plugin]  copy for tag mp4parser-project-1.0-RC-24

---
+++ tags/mp4parser-project-1.0-RC-24/trunk/isoparser/src/test/java/com/googlecode/mp4parser/authoring/InTestMovieCreator.java
@@ -0,0 +1,24 @@
+package com.googlecode.mp4parser.authoring;
+
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class InTestMovieCreator {
+    public static Movie createMovieOnlyVideo(String... names) throws IOException {
+        Movie m = new Movie();
+        for (String name : names) {
+            Movie m1 = MovieCreator.build(new FileInputStream(InTestMovieCreator.class.getProtectionDomain().getCodeSource().getLocation().getFile() + name).getChannel());
+            for (Track track : m1.getTracks()) {
+                if ("vide".equals(track.getHandler())) {
+                    m.addTrack(track);
+                }
+            }
+
+        }
+        return m;
+    }
+
+}

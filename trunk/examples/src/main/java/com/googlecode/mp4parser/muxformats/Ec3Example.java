d88973700c7ee82280cb7455a9f54d2b682891e8
Sebastian.Annies <sebastian.annies@googlemail.com>
2014-01-21 09:46:25 +0000

    make EC3TrackImpl more Java stylish - adopt DataSource default language to eng in TrackMetaData

--- trunk/examples/src/main/java/com/googlecode/mp4parser/muxformats/Ec3Example.java
+++ trunk/examples/src/main/java/com/googlecode/mp4parser/muxformats/Ec3Example.java
@@ -1,6 +1,7 @@
 package com.googlecode.mp4parser.muxformats;
 
 import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.FileDataSourceImpl;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
 import com.googlecode.mp4parser.authoring.tracks.EC3TrackImpl;
@@ -15,7 +16,7 @@ import java.nio.channels.FileChannel;
  */
 public class Ec3Example {
     public static void main(String[] args) throws IOException {
-        EC3TrackImpl track = new EC3TrackImpl(new FileInputStream("/home/sannies/Solekai020_1920_23_75x1_Dolby_v2/Dolby_Channel-Check_Voice_8-ch_Discrete_24bit_48kHz_dn24_448.ec3"));
+        EC3TrackImpl track = new EC3TrackImpl(new FileDataSourceImpl("C:\\Users\\sannies\\Downloads\\audio.ac3"));
         Movie m = new Movie();
         m.addTrack(track);
         DefaultMp4Builder mp4Builder = new DefaultMp4Builder();
